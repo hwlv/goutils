476b87720a88899afff9e17bb6776290bb21bf44
hongwang.lv <hongwang.lv@weimob.com>
2022-05-15 12:21:54 +0800

    add removeDuplicates function

--- arrutil/arr.go
+++ arrutil/arr.go
@@ -1,10 +1,20 @@
 package arrutil
 
-func contaions[T comparable](arr []T, e T) bool {
+func contains[T comparable](arr []T, e T) bool {
 	for _, v := range arr {
 		if v == e {
 			return true
 		}
 	}
 	return false
+}
+
+func removeDuplicates(strList []string) []string {
+	list := []string{}
+	for _, item := range strList {
+		if contains(list, item) == false {
+			list = append(list, item)
+		}
+	}
+	return list
 }
